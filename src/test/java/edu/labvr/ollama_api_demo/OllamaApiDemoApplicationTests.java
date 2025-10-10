03ff831ee5331fdc499964e205a46b741d9ecb42
monboga <monroygabriel9@gmail.com>
2025-10-09 21:32:20 -0600

    first commit

---
+++ src/test/java/edu/labvr/ollama_api_demo/OllamaApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.labvr.ollama_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
